b9e51d7767eb713a6fbecebf3661867955ddb63c
Mohammad Frotan <frotanm85@gmail.com>
2024-11-11 07:28:07 +0100

    all done

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// scripts.js
+document.getElementById('burger').addEventListener('click', function() {
+  var navMenu = document.getElementById('nav-menu');
+  navMenu.classList.toggle('nav-active');
+});
